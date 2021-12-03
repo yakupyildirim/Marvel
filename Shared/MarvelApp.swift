eeac7133308922dcc5fe303cc84c3dad9450790d
jacob <yakupyildirim@hotmail.com.tr>
2021-12-04 00:37:10 +0300

    Initial Commit

---
+++ Shared/MarvelApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelApp.swift
+//  Shared
+//
+//  Created by Yakup Yıldırım on 4.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct MarvelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
